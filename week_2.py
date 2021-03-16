4816663b1d21618e443fa5d68f054ca301677b1d
Eloi Bové <eloibove@gmail.com>
2021-03-16 19:40:29 +0100

    changes

--- week_2.py
+++ week_2.py
@@ -57,31 +57,32 @@ def main():
         bb_gt.append(boxes)
 
     print('\n\n------------------- Task 1: Gaussian models -------------------')
-    """
+    
     # Create gray model
     gestimator = GaussianBGEstimator(img_path, mask_path)
     gestimator.load_pretrained('models/gaussian.pkl')
 
     # Create color model
-    color_gestimator = ColorGaussianBGEstimator(img_path, mask_path)
-    color_gestimator.load_pretrained('models/rgb_independent.pkl')
+    #color_gestimator = ColorGaussianBGEstimator(img_path, mask_path)
+    #color_gestimator.load_pretrained('models/rgb_independent.pkl')
     
     # Test gray adaptive and non-adaptive
-    bb_ge = gestimator.test(vis=True, N_test_start = start, N_test_end = end)
+    #bb_ge = gestimator.test(vis=True, N_test_start = start, N_test_end = end)
     bb_gea = gestimator.test_adaptive(vis=True, N_test_start = start, N_test_end = end)
     
     # Test color adaptive and non-adaptive
-    bb_ge_color = color_gestimator.test(vis=True, N_test_end = 2000)
-    bb_gea_color = color_gestimator.test_adaptive(vis=True, N_test_end = 2000)
+    #bb_ge_color = color_gestimator.test(vis=True, N_test_end = 2000)
+    #bb_gea_color = color_gestimator.test_adaptive(vis=True, N_test_end = 2000)
 
 
     # Evaluate results
+    """
     map, _, _ = mean_average_precision(bb_gt, bb_ge)
     print('Gaussian gray estimator mAP: ' + str(map))
-
-    map, _, _ = mean_average_precision(bb_gt, bb_gea)
+    """
+    map, _, _ = mean_average_precision(bb_gt, bb_gea, method="area")
     print('Gaussian gray Adaptive estimator mAP: ' + str(map))
-
+    """
     map, _, _ = mean_average_precision(bb_gt, bb_ge_color)
     print('Gaussian color estimator mAP: ' + str(map))
 
@@ -90,6 +91,7 @@ def main():
     """
 
     print('\n\n------------------- Task 2: State of the art evaluation -------------------')
+    """
     # State of the art evaluation
     ocv_estimators = OpenCVBGEstimators(img_path, train_ratio=0.25)
     ocv_estimators.train(models=['MOG2', 'KNN'])
@@ -101,7 +103,7 @@ def main():
     bb_ocv_knn = ocv_estimators.test(model='KNN',N_test_start = start, N_test_end = end)
     map, _, _ = mean_average_precision(bb_gt, bb_ocv_knn)
     print('OCV bg subtraction KNN mAP: ' + str(map))  
-
+    """
 
 
     
