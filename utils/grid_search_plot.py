01d15181823f395fa28976d70faf71cf7186fad1
Arnau Roche <arnau.roche@gmail.com>
2021-04-12 21:21:22 +0200

    Plot for MSEN and PEPN added

--- utils/grid_search_plot.py
+++ utils/grid_search_plot.py
@@ -35,5 +35,103 @@ def main():
     maps_non_adaptive = np.linspace(0,4,num=4)
     grid_search_plot(maps_non_adaptive, maps_adaptive)
 
+
+
+def grid_search_plot2():
+    results = []   
+
+    x_axis = []
+    block_size = [4,8,16,32,64]
+    search_border = [2,4,8,16,32]
+
+    metric = [] # Search border x block_size
+
+    pepn = []
+    msen = []
+
+    for res in results:
+        pepn.append(res[7])
+        msen.append(res[6])
+        if res[0] == 'forward':
+            if res[4] == "SAD":
+                # block_size.append(res[1])
+                # search_border.append(res[2])
+                #metric.append(res[7]) # PEPN
+                metric.append(res[6]) # MSEN
+    
+    print(pepn)
+    print(msen)
+
+    pepn.sort()
+    msen.sort()
+
+    print("\n")
+    print(pepn)
+    print(msen)
+
+    BLOCK, SEARCH = np.meshgrid(block_size, search_border)
+    metric = np.reshape(metric, (5, 5))
+
+    
+
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(BLOCK, SEARCH, metric, rstride=1, cstride=1,
+                    cmap='viridis', edgecolor='none')
+    ax.set_xlabel('Block size')
+    ax.set_ylabel('Search border')
+    ax.set_zlabel('MSEN')
+    ax.set_title('Forward (SSD)')
+    plt.show()
+
+def grid_search_plot3():
+
+    results = []
+
+
+    x_axis = []
+    block_size = [4,8,16,32,64]
+    search_border = [2,4,8,16,32]
+
+    metric = [] # Search border x block_size
+
+    pepn = []
+    msen = []
+
+    for res in results:
+        pepn.append(res[7])
+        msen.append(res[6])
+        if res[0] == 'backward':
+            if res[4] == "template2":
+                # block_size.append(res[1])
+                # search_border.append(res[2])
+                metric.append(res[7]) # PEPN
+                #metric.append(res[6]) # MSEN
+    
+    print(pepn)
+    print(msen)
+
+    pepn.sort()
+    msen.sort()
+
+    print("\n")
+    print(pepn)
+    print(msen)
+
+    BLOCK, SEARCH = np.meshgrid(block_size, search_border)
+    metric = np.reshape(metric, (5, 5))
+
+
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.plot_surface(BLOCK, SEARCH, metric, rstride=1, cstride=1,
+                    cmap='viridis', edgecolor='none')
+    ax.set_xlabel('Block size')
+    ax.set_ylabel('Search border')
+    ax.set_zlabel('PEPN')
+    ax.set_title('Backward (CCOEFF_NORM)')
+    plt.show()
+    
 if __name__ == "__main__":
-    main()
+    #main()
+    grid_search_plot3()
